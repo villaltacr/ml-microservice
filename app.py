cb88ed4a7c9244e8b661b801c2a71fe2a0a2e699
dvillaltad <dvillalta@extraspace.com>
2020-06-05 14:58:54 -0600

    updating app.py

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 from flask import Flask, request, jsonify
-from flask.logging import create_logger
 import logging
-
 import pandas as pd
 from sklearn.externals import joblib
 from sklearn.preprocessing import StandardScaler
